c4fe5d887fc5e0c1dfce3dbbbb35606a6ecb731b
Adeyemo Ademola <adeyemoademola12@gmail.com>
2025-09-15 20:19:28 +0100

    `Update CORS origin to http://localhost:5173 in server.js`

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(helmet());
 // CORS Configuration
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:3000",
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
     credentials: true,
   })
 );
